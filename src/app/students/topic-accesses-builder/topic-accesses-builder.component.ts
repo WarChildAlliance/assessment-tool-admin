cef6e0de411d1b21a7254be7e1fb9b63d7a55cc7
Caio <caio.reliefapps@gmail.com>
2022-05-10 09:19:46 -0300

    changed conditions so checkbox will only be enabled when there are set dates

--- src/app/students/topic-accesses-builder/topic-accesses-builder.component.ts
+++ src/app/students/topic-accesses-builder/topic-accesses-builder.component.ts
@@ -23,8 +23,8 @@ export class TopicAccessesBuilderComponent implements OnInit {
   topicsList: Topic[] = [];
   selectedAssessmentId: string;
 
-  private startDate;
-  private endDate;
+  public startDate = null;
+  public endDate = null;
 
   private setDate: boolean;
 
