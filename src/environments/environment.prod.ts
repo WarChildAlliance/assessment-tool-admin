380f371ea6cb19f1052e00fae0b918eb20b35327
Harsh Mittal <harsh.reliefapps@gmail.com>
2022-10-04 14:40:50 +0000

    GOBEE-296: Added Login URL Column in Student's Table ✨

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://glmt-api.development.humanitarian.tech'
+  API_URL: 'https://glmt-api.development.humanitarian.tech',
+  STUDENT_PORTAL_LOGIN_URL: 'https://glmt-main.development.humanitarian.tech/auth'
 };
