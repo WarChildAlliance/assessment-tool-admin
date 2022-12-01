f509b7bb8593ba8d0a2e16667fe2892ebdb145b8
Hugo <hugo@reliefapplications.org>
2022-12-01 16:39:43 +0100

    add pacome changes

--- src/environments/environment.azure.ts
+++ src/environments/environment.azure.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://assessment-tool-api.westeurope.cloudapp.azure.com',
-  STUDENT_PORTAL_LOGIN_URL: 'https://glmt-main.development.humanitarian.tech/auth'
+  API_URL: 'https://api.gobee-education.org',
+  STUDENT_PORTAL_LOGIN_URL: 'https://play.gobee-education.org/auth'
 };
